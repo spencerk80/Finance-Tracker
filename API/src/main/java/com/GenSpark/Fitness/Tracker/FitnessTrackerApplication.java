94da380a425967877759919ba9921d88b1031b31
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-08-15 11:44:06 -0700

    Created start point of the project

---
+++ API/src/main/java/com/GenSpark/Fitness/Tracker/FitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.GenSpark.Fitness.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerApplication.class, args);
+	}
+
+}
