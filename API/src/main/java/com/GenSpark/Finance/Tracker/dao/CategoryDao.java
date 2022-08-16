d51229fc9ec421358a1aa1c26f33bf042b6a4492
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-08-16 14:35:43 -0700

    Fixed typo I didn't spot

--- API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
+++ API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
@@ -13,6 +13,6 @@ import java.util.Optional;
 public interface CategoryDao extends JpaRepository<Category, Integer> {
     Optional<Category> findByName(String name);
     Optional<Category> deleteByName(String name);
-    @Query(value = "SELECT * FROM category WHERE type = ?1"y, nativeQuery = true)
+    @Query(value = "SELECT * FROM category WHERE type = ?1", nativeQuery = true)
     List<Category> findByCategoryType(CategoryType type);
 }
