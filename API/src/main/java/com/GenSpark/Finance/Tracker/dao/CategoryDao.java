fbe208b0542a233f57f3c11e29b1e956bda0e9d6
Jack VanDyke <j.vandyket@gmail.com>
2022-08-16 10:35:32 -0400

    Initial DAOs without queries and refactored fitness to finance where applicable (if missed somewhere just refactor on your device for future push)

--- API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
+++ API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
@@ -3,7 +3,6 @@ package com.GenSpark.Finance.Tracker.dao;
 import com.GenSpark.Finance.Tracker.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Repository
 public interface CategoryDao extends JpaRepository<Category, Integer> {
