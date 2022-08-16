95f935bdc9a00f4a7495f8b449c27f2b812041a0
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-08-16 14:34:28 -0700

    Added custom queries

--- API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
+++ API/src/main/java/com/GenSpark/Finance/Tracker/dao/CategoryDao.java
@@ -1,14 +1,18 @@
 package com.GenSpark.Finance.Tracker.dao;
 
 import com.GenSpark.Finance.Tracker.entity.Category;
+import com.GenSpark.Finance.Tracker.enums.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryDao extends JpaRepository<Category, Integer> {
     Optional<Category> findByName(String name);
-
     Optional<Category> deleteByName(String name);
+    @Query(value = "SELECT * FROM category WHERE type = ?1"y, nativeQuery = true)
+    List<Category> findByCategoryType(CategoryType type);
 }
