840e6dc4accccf4c85edc958b2ff84dc72583a1b
Jack VanDyke <j.vandyket@gmail.com>
2022-08-22 21:22:06 -0400

    ffs pls let me finish this soon

--- react-app/src/components/Users/Users.tsx
+++ react-app/src/components/Users/Users.tsx
@@ -33,20 +33,20 @@ const Users = () => {
     };
   }, []);
 
-  return (
-    <article>
-      <h2>Users List</h2>
-      {users?.length ? (
-        <ul>
-          {users.map((user, i) => (
-            <li key={i}>{user?.username}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No users to display</p>
-      )}
-    </article>
-  );
+  //   return (
+  //     <article>
+  //       <h2>Users List</h2>
+  //       {users?.length ? (
+  //         <ul>
+  //           {users.map((user, i) => (
+  //             <li key={i}>{user?.username}</li>
+  //           ))}
+  //         </ul>
+  //       ) : (
+  //         <p>No users to display</p>
+  //       )}
+  //     </article>
+  //   );
 };
 
 export default Users;
