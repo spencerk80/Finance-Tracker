1a1490409519bcd8aa04acaef7f746f727519dda
Jack VanDyke <j.vandyket@gmail.com>
2022-08-24 00:18:06 -0400

    weird these dont go with vscode

--- API/src/main/java/com/GenSpark/Finance/Tracker/FinanceTrackerApplication.java
+++ API/src/main/java/com/GenSpark/Finance/Tracker/FinanceTrackerApplication.java
@@ -2,12 +2,25 @@ package com.GenSpark.Finance.Tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinanceTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:3000").allowCredentials(true);
+			}
+		};
 	}
 
 }
